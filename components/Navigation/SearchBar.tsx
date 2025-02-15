1bd951bf74de0a1d98e53016816596732ecf50fa
Taryn Thompson <78978422+taryn-t@users.noreply.github.com>
2025-02-15 13:05:53 -0500

    search bar placeholder

--- components/Navigation/SearchBar.tsx
+++ components/Navigation/SearchBar.tsx
@@ -6,9 +6,9 @@ export default function SearchBar(){
 
     return(
         <div className="w-full h-full grid place-items-center">
-            <div className="w-60 h-8 rounded-xl shadow-sm border-solid border-2 border-gray-300 grid grid-flow-col gap-1 place-items-center px-1">
+            <div className="w-60 h-8 rounded-xl shadow-sm border-solid border-2 border-gray-300 grid grid-flow-col gap-1 place-items-center py-0.5 px-1">
                 <IoIosSearch className="h-6 w-6 text-gray-400"/>
-                <input className="text-gray-500 w-full h-full border-none focus-visible:border-none focus-visible:ring-0 focus-visible:outline-none " />
+                <input placeholder="Search keywords or ISBN" className="text-gray-500 w-full h-full border-none focus-visible:border-none focus-visible:ring-0 focus-visible:outline-none " />
             </div>
         </div>
         
